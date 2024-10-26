2ebbd0c1248fcd6cd4f76625c92e48d2e19b8ed6
Raynard2 <148624014+Raynard2@users.noreply.github.com>
2024-10-26 14:39:18 -0400

    update app.py

--- src1/Frontend/app.py
+++ src1/Frontend/app.py
@@ -1,16 +1,13 @@
-from flask import Flask, render_template, flash, request, session
-
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main(): 
-    return "Hello World"
+def homepage():
+    return render_template('homepage.html')
 
 @app.route('/form')
 def form(): 
-    """ Report the live condition of disasters """
-
+    return 
 
 if __name__ == "__main__": 
     app.run(debug=True)
